7d323f29c222fcc0348a8815442512c41c636993
JavierAlejandroFatu <javieralejandrofatu@gmail.com>
2026-03-14 20:46:31 -0300

    feat(auth): añade pantalla de registro de usuario
    - Se crea componente Register
    - Se agrega formulario de registro
    - Validaciones básicas del formulario
    - Se agrega ruta /register en el routing
    - Integración inicial con AuthService
    - Se agrega configuración de environment para manejo de URLs

--- frontend/src/app/core/services/auth.ts
+++ frontend/src/app/core/services/auth.ts
@@ -1,12 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthServices {
-  apiUrl = '/api/auth';
+  apiUrl = environment.apiUrl;
   
   constructor(private httpClient: HttpClient){}
 
@@ -14,7 +15,7 @@ export class AuthServices {
     return this.httpClient.post<any>(`${this.apiUrl}/login`, { username, password });
   }
 
-  signUp(username: string, password: string): Observable<any> {
+  register(username: string, password: string): Observable<any> {
     return this.httpClient.post<any>(`${this.apiUrl}/register`, { username, password });
   }
 
