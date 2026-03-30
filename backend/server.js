951b5b3897ad7c4eb859cd545732c4b3a5e7ec6e
Ayrton <95386@sistemas.frc.utn.edu.ar>
2026-03-30 20:23:54 -0300

    fix: correcta precarga de tags

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,15 @@
 //Punto de entrada
 const app = require('./src/app');
 const { initDB } = require('./src/config/database');
+const { seedTags } = require('./src/seeds/tag.seed');
 
 const PORT = process.env.PORT || 3000;
 
 async function startServer() {
     try {
         await initDB();
+
+        await seedTags();
 
         app.listen(PORT, () => {
             console.log(`Servidor corriendo en el puerto ${PORT}`);
